bf8cd1803d49a2ce4d1e28a65a3e22e3e731d143
RazDV <raz.david.2002@gmail.com>
2025-12-25 14:34:00 +0200

    IntToBin v1

--- IntToBin.java
+++ IntToBin.java
@@ -8,7 +8,23 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+        String s = "";
+        String symbole = "";
+        if (x < 0) {
+            symbole = "-";
+            x = -x;
+        }
+        return symbole + toBinaryHelper(x, s);
     }    
+    
+    public static String toBinaryHelper(int x, String s) {
+        if (x == 0) return "0" + s;
+        if (x == 1) return "1" + s;
+
+        if ((x % 2) == 0) {
+            return toBinaryHelper(x/2, "0" + s);
+        } else {
+            return toBinaryHelper(x/2, "1" + s);
+        }
+    }
  }
